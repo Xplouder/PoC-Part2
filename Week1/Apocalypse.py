4b9ccafb552f67f36aa326ff6bc25bc988f71162
João Silva <jfasxx@hotmail.com>
2016-02-26 16:11:55 +0000

    Phase One done

---
+++ Week1/Apocalypse.py
@@ -0,0 +1,127 @@
+"""
+Student portion of Zombie Apocalypse mini-project
+"""
+
+import random
+import poc_grid
+import poc_queue
+import poc_zombie_gui
+
+# global constants
+EMPTY = 0
+FULL = 1
+FOUR_WAY = 0
+EIGHT_WAY = 1
+OBSTACLE = 5
+HUMAN = 6
+ZOMBIE = 7
+
+
+class Apocalypse(poc_grid.Grid):
+    """
+    Class for simulating zombie pursuit of human on grid with
+    obstacles
+    """
+
+    def __init__(self, grid_height, grid_width, obstacle_list=None,
+                 zombie_list=None, human_list=None):
+        """
+        Create a simulation of given size with given obstacles,
+        humans, and zombies
+        """
+        poc_grid.Grid.__init__(self, grid_height, grid_width)
+        if obstacle_list is not None:
+            for cell in obstacle_list:
+                self.set_full(cell[0], cell[1])
+        if zombie_list is not None:
+            self._zombie_list = list(zombie_list)
+        else:
+            self._zombie_list = []
+        if human_list is not None:
+            self._human_list = list(human_list)
+        else:
+            self._human_list = []
+
+    def clear(self):
+        """
+        Set cells in obstacle grid to be empty
+        Reset zombie and human lists to be empty
+        """
+        self._human_list = []
+        self._zombie_list = []
+        for row in xrange(self.get_grid_height()):
+            for col in xrange(self.get_grid_width()):
+                self.set_empty(row, col)
+
+    def add_zombie(self, row, col):
+        """
+        Add zombie to the zombie list
+        :param col:
+        :param row:
+        """
+        self._zombie_list.append((row, col))
+
+    def num_zombies(self):
+        """
+        Return number of zombies
+        """
+        return len(self._zombie_list)
+
+    def zombies(self):
+        """
+        Generator that yields the zombies in the order they were
+        added.
+        """
+        for zombie in self._zombie_list:
+            yield zombie
+
+    def add_human(self, row, col):
+        """
+        Add human to the human list
+        :param col:
+        :param row:
+        """
+        self._human_list.append((row, col))
+
+    def num_humans(self):
+        """
+        Return number of humans
+        """
+        return len(self._human_list)
+
+    def humans(self):
+        """
+        Generator that yields the humans in the order they were added.
+        """
+        for human in self._human_list:
+            yield human
+
+    def compute_distance_field(self, entity_type):
+        """
+        Function computes and returns a 2D distance field
+        Distance at member of entity_list is zero
+        Shortest paths avoid obstacles and use four-way distances
+        :param entity_type:
+        """
+        return
+
+    def move_humans(self, zombie_distance_field):
+        """
+        Function that moves humans away from zombies, diagonal moves
+        are allowed
+        :param zombie_distance_field:
+        """
+        pass
+
+    def move_zombies(self, human_distance_field):
+        """
+        Function that moves zombies towards humans, no diagonal moves
+        are allowed
+        :param human_distance_field:
+        """
+        pass
+
+# Start up gui for simulation - You will need to write some code above
+# before this will work without errors
+
+poc_zombie_gui.run_gui(Apocalypse(30, 40))
---
+++ Week1/ApocalypseTests.py
@@ -0,0 +1,69 @@
+import unittest
+import collections
+from Apocalypse import Apocalypse
+from Apocalypse import HUMAN
+
+
+class ApocalypseTests(unittest.TestCase):
+    def test_clear(self):
+        obj = Apocalypse(4, 6, [], [(1, 5), (3, 2)], [(0, 4), (2, 4), (3, 5)])
+        obj.clear()
+        expected_list = []
+
+        self.assertEqual(obj._human_list, expected_list)
+        self.assertEqual(obj._zombie_list, expected_list)
+        for row in xrange(obj.get_grid_height()):
+            for col in xrange(obj.get_grid_width()):
+                self.assertTrue(obj.is_empty(row, col))
+
+    def test_compute_distance_field_human(self):
+        obj = Apocalypse(3, 3, [], [], [(2, 2)])
+        result = obj.compute_distance_field(HUMAN)
+        expected = [[4, 3, 2],
+                    [3, 2, 1],
+                    [2, 1, 0]]
+
+        self.assertSequenceEqual(expected, result, seq_type=list)
+
+    def test_compute_distance_field_zombie(self):
+        obj = Apocalypse(3, 3, [], [(1, 1)], [])
+        result = obj.compute_distance_field(HUMAN)
+        expected = [[2, 1, 2],
+                    [1, 0, 1],
+                    [2, 1, 2]]
+
+        self.assertSequenceEqual(expected, result, seq_type=list)
+
+    def test_add_human(self):
+        obj = Apocalypse(3, 3, [], [(1, 1)], [])
+        obj.add_human(1, 1)
+        expected = 1
+
+        self.assertEqual(expected, obj.num_humans())
+
+    def test_add_zombie(self):
+        obj = Apocalypse(3, 3, [], [], [(1, 1)])
+        obj.add_zombie(1, 1)
+        expected = 1
+
+        self.assertEqual(expected, obj.num_zombies())
+
+    def test_move_human(self):
+        obj = Apocalypse(3, 3, [], [(2, 2)], [(1, 1)])
+        dist = [[4, 3, 2], [3, 2, 1], [2, 1, 0]]
+        obj.move_humans(dist)
+        generator = obj.humans()
+
+        self.assertTrue(isinstance(generator, collections.Iterable))
+
+    def test_move_zombies(self):
+        obj = Apocalypse(3, 3, [], [(2, 2)], [(1, 1)])
+        dist = [[2, 1, 2], [1, 0, 1], [2, 1, 2]]
+        obj.move_zombies(dist)
+        generator = obj.zombies()
+
+        self.assertTrue(isinstance(generator, collections.Iterable))
+
+
+if __name__ == '__main__':
+    unittest.main()
